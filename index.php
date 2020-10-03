549d83df327b920f2e0a09b297d9ed2643090b1f
GuilhermeSTrydeR <guilhermepereiramello@hotmail.com>
2020-10-03 12:53:41 -0300

    criando a primeira tela

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+
+//a variavel abaixo define a pagina selecionada ao clicar nos menus
+$pagina = (isset($_GET['pagina'])) ? $_GET['pagina'] : 'inicio';
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -13,15 +20,36 @@
 <body>
 
 
-<nav class="navbar navbar-light">
 
-    <div id="logo">
-        <a href="/ftp/">FTP</a>
-    </div>
 
-</nav>
+    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: 	#3b5998;">
+        <div id="logo">
+            <a href="/ftp/">FTP</a>
+        </div>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+          <ul class="navbar-nav">
+            <li class="nav-item <?= ($pagina == 'inicio')?'active':'' ?>">
+              <a class="nav-link" href="?pagina=inicio"><b>Início</b></a>
+            </li>
+            <li class="nav-item <?= ($pagina == 'sobre')?'active':'' ?>">
+              <a class="nav-link" href="?pagina=sobre"><b>Sobre</b></a>
+            </li>
+            <li class="nav-item <?= ($pagina == 'contato')?'active':'' ?>">
+              <a class="nav-link" href="?pagina=contato"><b>Contato</b></a>
+            </li>
+          </ul>
+
+        </div>
+      </nav>
     
-
-    
+    <?php
+        //esse include ira colocar na tela a pagina selecionada e que foi atribuida a variavel $pagina, assim sempre que uma pagina for atribuida a variavel $pagina, ela sera incluida abaixo
+        include("./paginas/$pagina.php");
+    ?>
+        
+        
 </body>
 </html>
---
+++ paginas/sobre.php
@@ -0,0 +1,5 @@
+<div class="row justify-content-md-center">
+
+    <p>test test</p>
+
+</div>
