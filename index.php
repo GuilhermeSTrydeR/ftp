3aefca54f72a3744252301124af438ad8ebf43f4
GuilhermeSTrydeR <guilhermepereiramello@hotmail.com>
2020-10-03 12:16:28 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema FTP</title>
+
+    <!--  bootstrap -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/ftp.css">
+
+</head>
+<body>
+
+
+<nav class="navbar navbar-light">
+
+    <div id="logo">
+        <a href="/ftp/">FTP</a>
+    </div>
+
+</nav>
+    
+
+    
+</body>
+</html>
