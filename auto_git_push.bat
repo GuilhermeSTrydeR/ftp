8bfedfaeeb8feecac39b5e94bd8cd88c68e39749
guilhermestryder <guilhermepereiramello@hotmail.com>
2021-03-02 12:07:52 -0300

    adicionado duas colunas no cadastro de usuario que serao armazenadas a data e hora do cadastro do usuario

--- auto_git_push.bat
+++ auto_git_push.bat
@@ -4,8 +4,6 @@ REM essa variavel ira receber o texto que sera digitado pelo usuario e usado com
 SET /P commit=escreva o commit:
 CLS
 
-git remote add origin https://github.com/guilhermestryder/ftp.git
-
 git add .
 git commit -m "%commit%"
 git push origin master
