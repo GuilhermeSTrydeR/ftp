2d986438a879ef34262ae8c459f822914e4ee9dc
João Gustavo Camilo Júnior <joaogcamilojr@gmail.com>
2021-04-13 18:24:39 -0300

    feat: add docker-compose and update sql file

--- database/ftp.sql
+++ database/ftp.sql
@@ -1,31 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 5.0.4
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 08-Mar-2021 às 13:24
--- Versão do servidor: 10.4.17-MariaDB
--- versão do PHP: 7.3.27
+CREATE DATABASE IF NOT EXISTS `ftp`;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Banco de dados: `ftp`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `config`
---
+USE `ftp`;
 
 CREATE TABLE `config` (
   `id` int(11) NOT NULL,
@@ -33,18 +8,8 @@ CREATE TABLE `config` (
   `descConfig` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Extraindo dados da tabela `config`
---
-
 INSERT INTO `config` (`id`, `valor`, `descConfig`) VALUES
 (1, 1, 'verificar se ja houve primeiro acesso no sistema');
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `contato`
---
 
 CREATE TABLE `contato` (
   `id` int(11) NOT NULL,
@@ -54,10 +19,6 @@ CREATE TABLE `contato` (
   `texto` varchar(255) DEFAULT NULL,
   `excluido` varchar(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Extraindo dados da tabela `contato`
---
 
 INSERT INTO `contato` (`id`, `nome`, `email`, `telefone`, `texto`, `excluido`) VALUES
 (1, 'guilherme silva', 'a@a', 'xxx-xxxx', 'asdasdads', '1'),
@@ -72,12 +33,6 @@ INSERT INTO `contato` (`id`, `nome`, `email`, `telefone`, `texto`, `excluido`) V
 (10, 'guilherme silva', 'a@a', 'xxx-xxxx', 'ta faltando uma table aqui \r\n   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!', '1'),
 (11, 'guilherme silva', 'a@a', 'xxx-xxxx', 'teste', '1'),
 (12, 'guilherme silva', 'a@a', 'xxx-xxxx', 'asdsdasdasd', '0');
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `usuarios`
---
 
 CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL,
@@ -95,10 +50,6 @@ CREATE TABLE `usuarios` (
   `excluido` varchar(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Extraindo dados da tabela `usuarios`
---
-
 INSERT INTO `usuarios` (`id`, `nome`, `email`, `user`, `pass`, `permissao`, `status`, `tempo`, `telefone`, `dataCadastro`, `dataCadastroUnix`, `idAdm`, `excluido`) VALUES
 (1, 'guilherme silva', 'a@a', 'admin', '21232f297a57a5a743894a0e4a801fc3', '3', '1', 1, 'xxx-xxxx', NULL, NULL, NULL, '0'),
 (2, 'teste temporario', '1@1', 'adminteste', '21232f297a57a5a743894a0e4a801fc3', '3', '2', 1615037933, '123123', '20210304103853', '1614865133', 1, '1'),
@@ -106,51 +57,20 @@ INSERT INTO `usuarios` (`id`, `nome`, `email`, `user`, `pass`, `permissao`, `sta
 (4, 'asdasdasd@sadasd', 'sasd@sadasd', 'admincsadsad', '21232f297a57a5a743894a0e4a801fc3', '3', '2', 1614955656, '1223123', '20210304104736', '1614865656', 1, '1'),
 (5, 'seu zé', 'seuze@hamburguinho.com', 'ze', '98b456a0723fa616284a632d9d31821b', '1', '2', 1614887762, '8888-8888', '20210304145602', '1614880562', 1, '1');
 
---
--- Índices para tabelas despejadas
---
-
---
--- Índices para tabela `config`
---
 ALTER TABLE `config`
   ADD PRIMARY KEY (`id`);
 
---
--- Índices para tabela `contato`
---
 ALTER TABLE `contato`
   ADD PRIMARY KEY (`id`);
 
---
--- Índices para tabela `usuarios`
---
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT de tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `config`
---
 ALTER TABLE `config`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
---
--- AUTO_INCREMENT de tabela `contato`
---
 ALTER TABLE `contato`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
---
--- AUTO_INCREMENT de tabela `usuarios`
---
 ALTER TABLE `usuarios`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
