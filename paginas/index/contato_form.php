6393675d4242992206e0edaae30585958c499d3e
GuilhermeSTrydeR <guilhermepereiramello@hotmail.com>
2021-04-23 12:14:07 -0300

    arrumado o leadout do contato para usuarios logados

--- paginas/index/contato_form.php
+++ paginas/index/contato_form.php
@@ -31,7 +31,7 @@
                 </div>
                 <br>
                 <div class="form-group">
-                    <textarea maxlength ="1000" class="form-control" id="text" rows="6" name="texto" required ></textarea>
+                    <textarea maxlength ="1000" class="form-control" id="text" rows="6" name="texto" placeholder="Sua mensagem" style="resize: none" required ></textarea>
                 </div>
                 <br>
                 <div id="actions" class="row">
@@ -48,34 +48,30 @@
             else{
         ?>
     <!-- abaixo foram inseridas algumas variaveis $_SESSION para retornar, nome, email e telefone, essas variaveis foram preenchidas na classe usuario/logar-->
-    <form method="POST" action="/classes/contato/gravar_contato.php" class="needs-validation" novalidate>
-        <p>Preencha o formulario de contato abaixo:</p>
-            <div class="form-row">
-                <div class="col-md-4 mb-3">
-                <!-- <label for="validationCustom01">Nome</label> -->
-                <input type="text" readonly class="form-control" id="" name="nome" placeholder="<?php echo $_SESSION['nome']; ?>" value="<?php echo $_SESSION['nome']; ?>" required>
+
+    <form action="../../classes/contato/gravar_contato.php" method="POST" style='max-width: 500px; margin-top: 50px;'>
+            <p>Preencha o formulario de contato abaixo:</p>
+                <div class="form-group">
+                    <input type="text" readonly class="form-control" id="nome" name="nome" required placeholder='<?php echo $_SESSION['nome']; ?>' value="<?php echo $_SESSION['nome']; ?>">
                 </div>
-                <div class="col-md-4 mb-3">
-                <!-- <label for="validationCustom02">E-mail</label> -->
-                <input type="email" readonly class="form-control" id="" name="email" placeholder="<?php echo $_SESSION['email']; ?>" value="<?php echo $_SESSION['email']; ?>" required>
-                </div>
-                <div class="col-md-4 mb-3">
-                <!-- <label for="validationCustom04">telefone</label> -->
-                <input type="text" readonly class="form-control" id="" name="telefone" placeholder="<?php echo $_SESSION['telefone'];?>" value="<?php echo $_SESSION['telefone']; ?>" required>
-                </div>
-                <div class="col-md-3 mb-3">
-                <!-- <label for="validationCustom05">Mensagem</label> -->
                 <br>
-                <textarea name="texto" class="form-control" id="" cols="48" rows="4" placeholder="Sua mensagem" required style="margin-top: -25px; height: 200px; width: 457px; margin-left: -57px; resize: none;"></textarea>
-            </div>
-        </div>
-        <div id="actions" class="row">
-            <div class="col-md-12"> <button type="submit" class="btn btn-primary">Salvar</button> 
-                <a style='color: white !important' href="?pagina=inicio" class="btn btn-danger">Cancelar</a> </div>
-            </div>
-        </form>
-
-
+                <div class="form-group">
+                    <input type="email" readonly class="form-control" id="email" name="email" required placeholder="<?php echo $_SESSION['email']; ?>" value="<?php echo $_SESSION['email']; ?>">
+                </div>
+                <br>
+                <div class="form-group">
+                    <input type="text" readonly class="form-control" id="telefone" name="telefone" required placeholder='Telefone' placeholder="<?php echo $_SESSION['telefone'];?>" value="<?php echo $_SESSION['telefone']; ?>">
+                </div>
+                <br>
+                <div class="form-group">
+                    <textarea maxlength ="1000" class="form-control" id="text" rows="6" name="texto" placeholder="Sua mensagem" style="resize: none" required ></textarea>
+                </div>
+                <br>
+                <div id="actions" class="row">
+                    <div class="col-md-12"> <button type="submit" class="btn btn-primary">Salvar</button> 
+                    <a style='color: white !important' href="?pagina=inicio" class="btn btn-danger">Cancelar</a> </div>
+                </div>
+            </form>
         <?php 
         
             }
