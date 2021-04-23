b850a9be0ed9c649bb68a3ab3d33037392c76904
GuilhermeSTrydeR <guilhermepereiramello@hotmail.com>
2021-04-23 11:28:17 -0300

    alguns ajustes no lead-out das tables

--- paginas/configs/configUser.php
+++ paginas/configs/configUser.php
@@ -1,5 +1,5 @@
 <?php
-    if(!isset($_SESSION['logado']) || $_SESSION['permissao'] != '3'){
+    if(!isset($_SESSION['logado'])){
 
         header("Location: /");
 
@@ -11,8 +11,6 @@
 
 <center style="margin-left: 60px; margin-top: 100px !important;">
 <hr />
-    
-        
     <h2>Alterar sua Senha</h2>
     <form action="../../classes/usuario/alterarSenha.php" autocomplete="off" method="POST" style="margin-left: 300px;">
         <div class="row">    
@@ -21,19 +19,18 @@
             
         <div id="actions" class="row">
             <div class="col-md-12"> <button type="submit" class="btn btn-primary">Salvar</button> 
-            <a href="?pagina=inicio" class="btn btn-danger">Cancelar</a> </div>
+            <a href="?pagina=inicio" class="btn btn-danger" style="color: white !important;">Cancelar</a> </div>
         </div>
     </form>
 <hr />
-<br><br>
-<h2>Alterar seu nome de exibição</h2>
+<h2 style="margin-left: -100px;">Alterar seu nome de exibição</h2>
     <form action="../../classes/usuario/alterarNome.php" autocomplete="off" method="POST">
         <div class="row">    
             <div class="form-group col-md-3"> <label for="user">Novo nome</label> <input type="text" class="form-control" name="nome" required autocomplete="off"> 
         </div>
         <div id="actions" class="row">
             <div class="col-md-12"> <button type="submit" class="btn btn-primary">Salvar</button> 
-            <a href="?pagina=inicio" class="btn btn-danger">Cancelar</a> </div>
+            <a href="?pagina=inicio" class="btn btn-danger" style="color: white !important;">Cancelar</a> </div>
         </div>
     </form>
 <hr />
