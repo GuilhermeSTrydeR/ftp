0e74c9e4152eabbb7454bf03d819cf3b1fe35237
unknown <guilhermepereiramello@hotmail.com>
2021-06-03 14:42:50 -0300

    hhhhh

--- database/ftp.sql
+++ database/ftp.sql
@@ -1,15 +1,50 @@
-CREATE DATABASE IF NOT EXISTS `ftp`;
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 03-Jun-2021 às 19:41
+-- Versão do servidor: 10.4.19-MariaDB
+-- versão do PHP: 7.3.28
 
-USE `ftp`;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE IF NOT EXISTS `config` (
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `ftp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `config`
+--
+
+CREATE TABLE `config` (
   `id` int(11) NOT NULL,
   `valor` int(11) DEFAULT NULL,
   `descConfig` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Extraindo dados da tabela `config`
+--
+
 INSERT INTO `config` (`id`, `valor`, `descConfig`) VALUES
 (1, 1, 'verificar se ja houve primeiro acesso no sistema');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `contato`
+--
 
 CREATE TABLE `contato` (
   `id` int(11) NOT NULL,
@@ -19,6 +54,10 @@ CREATE TABLE `contato` (
   `texto` varchar(255) DEFAULT NULL,
   `excluido` varchar(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `contato`
+--
 
 INSERT INTO `contato` (`id`, `nome`, `email`, `telefone`, `texto`, `excluido`) VALUES
 (1, 'guilherme silva', 'a@a', 'xxx-xxxx', 'asdasdads', '1'),
@@ -32,7 +71,14 @@ INSERT INTO `contato` (`id`, `nome`, `email`, `telefone`, `texto`, `excluido`) V
 (9, 'guilherme silva', 'a@a', 'xxx-xxxx', 'precisa arruma essa , pois não ta quebrando linha, saca só: ###################################################################################################################################################################################################', '1'),
 (10, 'guilherme silva', 'a@a', 'xxx-xxxx', 'ta faltando uma table aqui \r\n   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!', '1'),
 (11, 'guilherme silva', 'a@a', 'xxx-xxxx', 'teste', '1'),
-(12, 'guilherme silva', 'a@a', 'xxx-xxxx', 'asdsdasdasd', '0');
+(12, 'guilherme silva', 'a@a', 'xxx-xxxx', 'asdsdasdasd', '0'),
+(13, 'SEU ZÉ', 'A@A', 'MUITO RUIM', 'NÃO GOSTEI DO SITE', '0');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuarios`
+--
 
 CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL,
@@ -47,30 +93,68 @@ CREATE TABLE `usuarios` (
   `dataCadastro` varchar(64) DEFAULT NULL,
   `dataCadastroUnix` varchar(64) DEFAULT NULL,
   `idAdm` int(11) DEFAULT NULL,
-  `excluido` varchar(1) DEFAULT NULL
+  `excluido` varchar(1) DEFAULT NULL,
+  `ativo` int(1) DEFAULT 1,
+  `setor` int(64) DEFAULT NULL,
+  `nasc` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO `usuarios` (`id`, `nome`, `email`, `user`, `pass`, `permissao`, `status`, `tempo`, `telefone`, `dataCadastro`, `dataCadastroUnix`, `idAdm`, `excluido`) VALUES
-(1, 'guilherme silva', 'a@a', 'admin', '21232f297a57a5a743894a0e4a801fc3', '3', '1', 1, 'xxx-xxxx', NULL, NULL, NULL, '0'),
-(2, 'teste temporario', '1@1', 'adminteste', '21232f297a57a5a743894a0e4a801fc3', '3', '2', 1615037933, '123123', '20210304103853', '1614865133', 1, '1'),
-(3, 'teste', 'a@a', 'teste2222', '21232f297a57a5a743894a0e4a801fc3', '3', '2', 1614869223, '13123123', '20210304104703', '1614865623', 1, '1'),
-(4, 'asdasdasd@sadasd', 'sasd@sadasd', 'admincsadsad', '21232f297a57a5a743894a0e4a801fc3', '3', '2', 1614955656, '1223123', '20210304104736', '1614865656', 1, '1'),
-(5, 'seu zé', 'seuze@hamburguinho.com', 'ze', '98b456a0723fa616284a632d9d31821b', '1', '2', 1614887762, '8888-8888', '20210304145602', '1614880562', 1, '1');
+--
+-- Extraindo dados da tabela `usuarios`
+--
 
+INSERT INTO `usuarios` (`id`, `nome`, `email`, `user`, `pass`, `permissao`, `status`, `tempo`, `telefone`, `dataCadastro`, `dataCadastroUnix`, `idAdm`, `excluido`, `ativo`, `setor`, `nasc`) VALUES
+(1, 'guilherme silva', 'a@a', 'admin', '21232f297a57a5a743894a0e4a801fc3', '3', '1', 1, '(35) 9999-9999', NULL, NULL, NULL, '0', 1, 5, '1994-01-31'),
+(2, 'teste temporario', '1@1', 'adminteste', '21232f297a57a5a743894a0e4a801fc3', '3', '2', 1615037933, '123123', '20210304103853', '1614865133', 1, '1', 1, NULL, NULL),
+(3, 'teste', 'a@a', 'teste2222', '21232f297a57a5a743894a0e4a801fc3', '3', '2', 1614869223, '13123123', '20210304104703', '1614865623', 1, '1', 1, NULL, NULL),
+(4, 'asdasdasd@sadasd', 'sasd@sadasd', 'admincsadsad', '21232f297a57a5a743894a0e4a801fc3', '3', '2', 1614955656, '1223123', '20210304104736', '1614865656', 1, '1', 1, NULL, NULL),
+(5, 'seu zé', 'seuze@hamburguinho.com', 'ze', '98b456a0723fa616284a632d9d31821b', '1', '2', 1614887762, '8888-8888', '20210304145602', '1614880562', 1, '1', 1, NULL, NULL);
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `config`
+--
 ALTER TABLE `config`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Índices para tabela `contato`
+--
 ALTER TABLE `contato`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Índices para tabela `usuarios`
+--
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id`);
 
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `config`
+--
 ALTER TABLE `config`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
+--
+-- AUTO_INCREMENT de tabela `contato`
+--
 ALTER TABLE `contato`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
+--
+-- AUTO_INCREMENT de tabela `usuarios`
+--
 ALTER TABLE `usuarios`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
