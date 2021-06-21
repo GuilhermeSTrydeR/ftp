a0c34f443292275811bd32a0fcae586a388a89d3
GuilhermeSTrydeR <guilhermepereiramello@hotmail.com>
2021-06-21 17:41:58 -0300

    mais alteracoes de leadout

--- paginas/index/inicio.php
+++ paginas/index/inicio.php
@@ -1,4 +1,4 @@
 <center>
-
+    <img src="../../imagens/logotipo.png" alt="" style='margin-top: 150px;'>
 
 </center>
--- paginas/index/sobre.php
+++ paginas/index/sobre.php
@@ -7,21 +7,24 @@
 </center>
 <br><br>
     <div class='row'>
-        <div class='col' style='margin-left: 100px; max-width: 500px;'>
+        <div class='col' style='margin-left: 150px; max-width: 500px;'>
             
             <p align="justify" >
-
-                O <b>FTP</b> tem como principal finalidade atuar como um sistema onde é possivel cadastrar e visualizar fichas tecnicas de produto de forma digital(por meio de um computador), visando assim a redução de tempo e possivelmente um incremento de produtividade.
-
+                Elimine as fichas tecnicas impressas em papel e começe a utiliza-las no meio digitai, com isso  Tenha mais segurança e autonomia de suas fichas tecnicas em um só lugar, basta entrar com seu usuario e começar a usar.
+        
             </p>
+          
         </div>
 
-        <div class='col' style='margin-left: 150px; max-width: 500px;'>
+        <div class='col' style='margin-left: 50px; max-width: 500px;'>
 
-            <p align="justify" >
+        <p align="justify" >
+            
+            O <b>FTP</b> tem como principal finalidade atuar como um sistema onde é possivel cadastrar e visualizar fichas tecnicas de produto de forma digital(por meio de um computador), visando assim a redução de tempo e possivelmente um incremento de produtividade.
+        
 
+        </p>
 
-            </p>
         </div>
     
     
